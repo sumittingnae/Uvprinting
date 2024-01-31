7d16675a5876b2b7f38ed72ad10ae56371c88815
unknown <sumittingane18@gmail.com>
2024-02-01 00:48:45 +0530

    Create a React app and publish it to GitHub Pages

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './footer.scss'
+import './footer.scss';
+import {Link} from 'react-router-dom'
 
 
  const footer=()=>{
@@ -13,11 +14,113 @@ import './footer.scss'
                 <div className="col-lg-5 w-100">
                   <div className="footers">
                     <div className="row">
-                      <div className="col-3">dddd</div>
-                      <div className="col-3">dddd</div>
-                      <div className="col-3">dddd</div>
-                      <div className="col-3">dddd</div>
-                      
+                      <div className="col-3 colu-1">
+                        <div className="logo">
+                          <img src="./template/logo.png" />
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                          </p>
+                          <div className="social-media">
+                            <i
+                              className="fa-brands fa-facebook-f fb"
+                              style={{ background: "#ff4073" }}
+                            ></i>
+                            <i
+                              className="fa-brands fa-twitter"
+                              style={{ background: "#01d5c9" }}
+                            ></i>
+                            <i
+                              className="fa-brands fa-whatsapp"
+                              style={{ background: "#ffc400" }}
+                            ></i>
+                            <i
+                              className="fa-brands fa-linkedin"
+                              style={{ background: "#01d5c9" }}
+                            ></i>
+                            <i
+                              className="fa-brands fa-youtube"
+                              style={{ background: "#ff4073" }}
+                            ></i>
+                            {/* //<i className="fa-brands fa-facebook"></i> */}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="col-3 colu-2">
+                        <h6>Company</h6>
+                        <ul>
+                          <li>
+                            <Link to="/about" id="menu">
+                              About us
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/shop" id="menu">
+                              Shop
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/blog" id="menu">
+                              Blog
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/contact" id="menu">
+                              Contact
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="col-3 colu-2 colu-3">
+                        <h6>Company</h6>
+                        <ul>
+                          <li>
+                            <Link to="/about" id="menu">
+                              About us
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/shop" id="menu">
+                              Shop
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/blog" id="menu">
+                              Blog
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/contact" id="menu">
+                              Contact
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="col-3 colu-2 colu-4">
+                        <h6>Company</h6>
+                        <ul>
+                          <li>
+                            <Link to="/about" id="menu">
+                              About us
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/shop" id="menu">
+                              Shop
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/blog" id="menu">
+                              Blog
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/contact" id="menu">
+                              Contact
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,16 +1,104 @@
 import React from 'react';
 import Header from '../../component/header/header';
+import './Contact.scss';
+import GoogleMap from './googlemap';
+import Footer from '../../component/footer/footer';
 
  const Contact=()=>{
 
-    return(
+    return (
+      <>
+        <Header />
+        <div className="contact">
+          <div className="container">
+            <div className="wrapper">
+              <div className="row col-6 col-12">
+                <div className="col-lg-6 w-100">
+                  <div className="contents">
+                    <div className="left-side">
+                      <p>CONTACT US</p>
+                      <h6>Get In Touch</h6>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aliquam a ullamcorper ligula. Etiam hendrerit sodales
+                        erat, ac imperdiet odio venenatis eu. Donec lacinia
+                        augue nec diam consequat iaculis. Morbi in egestas arcu.
+                      </p>
 
-        <>
-          <Header/>  
-          Hello Contact
-        </>
+                      <div className="social-media">
+                        <i
+                          className="fa-brands fa-facebook-f fb"
+                          style={{ background: "#ff4073" }}
+                        ></i>
+                        <i
+                          className="fa-brands fa-twitter"
+                          style={{ background: "#01d5c9" }}
+                        ></i>
 
-    )
+                        <i
+                          className="fa-brands fa-linkedin"
+                          style={{ background: "#01d5c9" }}
+                        ></i>
+
+                        {/* //<i className="fa-brands fa-facebook"></i> */}
+                      </div>
+                    </div>
+                    <div className="right-side">
+                      <h6>NEED TO MAKE AN ENQUIRY?</h6>
+                      <form>
+                        <div className="form-group">
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your Name"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter your Name"
+                          />
+                        </div>
+                      </form>
+                      <button className='btn'>Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <GoogleMap/>
+        <Footer/>
+      </>
+    );
 
 }
 export default Contact;
